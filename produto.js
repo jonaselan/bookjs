7d25962dbcba994bff55cfa1179c6b5f490ecf8a
jonaselan <jonas.elan@gmail.com>
2018-06-04 20:32:22 -0300

    Adicionar comentários

--- produto.js
+++ produto.js
@@ -1,7 +1,10 @@
+// adicionar lib para levantar servidor
 var http = require('http');
 
+// há requisições e respostas
 http.createServer(function(request, response){
 	response.end('sim');
-}).listen(3000, 'localhost');
+}).listen(3000, 'localhost'); // criar servidor q escuta na porta 3000
 
+// msg no terminal
 console.log('servidor rodando');
