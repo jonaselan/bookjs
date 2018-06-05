8090330462828c5b541b489b925af40287591ea4
jonaselan <jonas.elan@gmail.com>
2018-06-04 22:56:23 -0300

    Criar variavel para iterar na view

--- app/routes/books.js
+++ app/routes/books.js
@@ -1,7 +1,15 @@
+var db_books = [
+  { id: 1, name: 'livro1'},
+  { id: 2, name: 'livo2' }
+];
+
 module.exports = function(app){
   app.get('/livros', function(req, res){ // primeira rota
+    // simular busca ao banco
+    // console.log(db_books);
+
     // render procura pela pasta views
-    res.render('books/index');
+    res.render('books/index', {books: db_books});
     // res.send('books/index');
   });
 }
