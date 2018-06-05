4322da1c6624b27fa08dcc3cbc4bee1e061a6fe0
jonaselan <jonas.elan@gmail.com>
2018-06-04 21:02:17 -0300

    Adicionar ejs
    adicionar essa lib para facilitar a criação de páginas

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express(); // representa o express
 
+// definir variaveis dentro do express e que passa peli sistema inteiro
+app.set('view engine', 'ejs');
+
 app.get('/livros', function(req, res){ // primeira rota
-  res.send('lista de livros');
+  res.render('books/index');
+  // res.send('books/index');
 });
 
 app.listen(3000, function(){
